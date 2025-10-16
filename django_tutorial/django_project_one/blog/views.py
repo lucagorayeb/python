ced81e1399f379399a29592d3617d24deb2f6fae
Luca Gorayeb <lucagorayeb@gmail.com>
2025-10-16 15:10:05 -0400

    updanting

--- django_tutorial/django_project_one/blog/views.py
+++ django_tutorial/django_project_one/blog/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+            {
+                'author': 'Luca',
+                'title': 'Blog Post 1',
+                'content': 'First post content',
+                'date_posted': 'Octuber 14, 2025'
+            },
+
+            {
+                'author': 'Larissa',
+                'title': 'Blog Post 2',
+                'content': 'Second post content',
+                'date_posted': 'Octuber 15, 2025'
+
+            }
+        ]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+            'posts': posts        
+        }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
