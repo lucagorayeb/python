5a38af094b0bbd34d2dca1c95be81c8137f6fc3c
Luca Siqueira Assis Gorayeb de Mello <lucagorayeb@gmail.com>
2025-10-29 13:16:57 -0400

    automatizando umas mudaças de arquivos dos meus estudos de java

---
+++ automação/terminal/corrigindo_arquivos_3.py
@@ -0,0 +1,46 @@
+import pyautogui
+import time as t
+
+def sleep():
+    t.sleep(1)
+
+def enter():
+    pyautogui.press('enter')
+
+def mudar_arquivos_exercicios():
+    pyautogui.write('cd Documentos/java/exercicios/beecrowd/iniciante/')
+    enter()
+    sleep()
+    mudancas = ['area','calculosimples','consumo','diferenca','esfera', 'helloworld', 'lanche',  'media2', 'produtosimples', 'selecao', 'teste'
+                ,'areacirculo' , 'cedulas', 'conversaotempo', 'distancia', 'extremamentebasico', 'idadedias', 'maior', 'media3', 'salario', 'somasimples',
+                'triangulo', 'bhaskara', 'cedulasmoedas', 'coordenadas', 'distanciaentredoispontos', 'gastocombustivel', 'intervalo', 'media1', 'multiplos',
+                'salariocombonus', 'sort_simples']
+    repeticao(mudancas)
+
+def repeticao(mudancas):
+    for mudanca in mudancas:
+        pyautogui.write(f'cd {mudanca}')
+        enter()
+        sleep()
+        pyautogui.write('mv lib/ domain/')
+        sleep()
+        enter()
+        sleep()
+        pyautogui.write('mv domain/ src/')
+        sleep()
+        enter()
+        sleep()
+        pyautogui.write('cd ..')
+        sleep()
+        enter()
+        sleep()
+
+def abrir_novo_terminal():
+    sleep()
+    pyautogui.hotkey('ctrl','t')
+    sleep()
+    pyautogui.click()
+    sleep()
+    mudar_arquivos_exercicios()
+
+abrir_novo_terminal()
