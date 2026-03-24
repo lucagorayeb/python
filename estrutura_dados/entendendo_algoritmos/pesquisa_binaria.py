37c07342c0f5e94b7cad737825e98533b39cc171
Luca Gorayeb <lucagorayeb@gmail.com>
2026-03-23 23:11:00 -0400

    -

---
+++ estrutura_dados/entendendo_algoritmos/pesquisa_binaria.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# A pesquisa binária é um algoritmo que faz buscas com o tempo de execução
+# log N na base 2, o que torna ela bem rápida.
+
+import random
+
+def pesquisa_binaria(array, item):
+    baixo = 0 
+    alto = len(array) - 1
+
+    while baixo <= alto:
+        meio = int((baixo + alto)/2)
+        chute = array[meio]
+
+        if chute == item:
+            return f"Parabéns você acertou. O número era {chute}"
+        
+        elif chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+            
+    
+    return None
+
+def sorteia_item(array):
+    return random.choice(array)
+
+array = [1,2,3,4,5,6,7,8,9,10]
+item = sorteia_item(array)
+#item = 7
+print(pesquisa_binaria(array, item))
+
