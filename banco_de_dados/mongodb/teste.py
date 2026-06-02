47409d115079bb165d217b8f2096f1f049104ac1
Luca Gorayeb <lucagorayeb@gmail.com>
2026-06-02 18:37:11 -0400

    Removing trash

--- banco_de_dados/mongodb/teste.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-
-data_string = "29/06/2015"
-data = datetime.strptime(data_string, "%d/%m/%Y")
-idade = (datetime.now() - data).days / 365
-idade_formatada = int(f'{idade:.0f}')
-
-print(type(idade_formatada))
--- sockets/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c.connect(('192.168.0.209',4000))
-
-while True:
-    mensagem = input('Enter mensage: ')
-    c.send(mensagem.encode("utf-8")[:2048])
-    
